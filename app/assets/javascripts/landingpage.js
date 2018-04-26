5802fa3539a8bd9d7b0318145a3a80045403fdf0
rayane <rayanesantos-131@hotmail.com>
2018-04-26 02:17:18 -0300

    inicial-commit-with-setup-and-template

---
+++ app/assets/javascripts/landingpage.js
@@ -0,0 +1,10 @@
+//= require landingpage/modernizr-2.6.2.min.js
+
+//= require landingpage/jquery.min.js
+//= require landingpage/jquery.easing.1.3.js
+//= require landingpage/bootstrap.min.js
+//= require landingpage/jquery.waypoints.min.js
+//= require landingpage/jquery.stellar.min.js
+//= require landingpage/owl.carousel.min.js
+//= require landingpage/main.js
+

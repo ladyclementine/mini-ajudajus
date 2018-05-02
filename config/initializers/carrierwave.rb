b917c31e97b963ce473c14a1fa0eb52fdc07858e
rayane <rayanesantos-131@hotmail.com>
2018-05-01 21:40:24 -0300

    carriewave-config-2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,18 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-      provider:              'AWS',                        # required
-      aws_access_key_id:     Rails.application.secrets.s3_access_key_id,            # required
-      aws_secret_access_key: Rails.application.secrets.s3_secret_access_key,            # required
-      region:                'sa-east-1',                  # optional, defaults to 'us-east-1'
+  if Rails.env.test?
+    config.storage :file
+    config.asset_host = 'http://localhost:3000'
+  else
+    config.storage = :fog
+    config.fog_use_ssl_for_aws = true
+    config.fog_directory  = 'miniajudajus'
+    config.fog_public     = true
+    config.fog_attributes = { 'Cache-Control': 'max-age=315576000' }
+
+    config.fog_credentials = {
+      provider:               'AWS'
+      aws_access_key_id:      Rails.application.secrets.s3_access_key_id,  
+      aws_secret_access_key:  Rails.application.secrets.s3_secret_access_key, 
     }
-    config.fog_directory  = 'ecej17'                          # required
-    config.fog_public     = true                                        # optional, defaults to true
-    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+  end
 end

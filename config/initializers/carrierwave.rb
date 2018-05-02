2c2fdbd2aa04fe6ceb8a493774d8a06761763c75
rayane <rayanesantos-131@hotmail.com>
2018-05-01 21:59:24 -0300

    carriewave-config-10

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => Rails.application.secrets.s3_access_key_id,,
+      :aws_access_key_id     => Rails.application.secrets.s3_access_key_id,
       :aws_secret_access_key => Rails.application.secrets.s3_secret_access_key,
-      :region                => 'sa-east-1'
+      :region                => 'sa-east-1',
     }
     config.fog_directory     =>  :fog
   end
